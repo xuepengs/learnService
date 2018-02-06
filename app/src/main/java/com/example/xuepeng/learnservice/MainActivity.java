b44ec387911957ae3e36a157041896dd26b0aaa9
xuepeng <helloxuepeng@gmail.com>
2018-02-06 11:00:14 +0800

    绑定service

--- app/src/main/java/com/example/xuepeng/learnservice/MainActivity.java
+++ app/src/main/java/com/example/xuepeng/learnservice/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.xuepeng.learnservice;
 
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, ServiceConnection {
     private Intent intent;
 
     @Override
@@ -14,18 +18,39 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
         intent=new Intent(MainActivity.this,MyService.class);
-        findViewById(R.id.startService).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        findViewById(R.id.startService).setOnClickListener(this);
+        findViewById(R.id.stopService).setOnClickListener(this);
+        findViewById(R.id.bindService).setOnClickListener(this);
+        findViewById(R.id.unBindService).setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.startService:
                 startService(intent);
-            }
-        });
-        findViewById(R.id.stopService).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+                break;
+            case R.id.stopService:
                 stopService(intent);
-            }
-        });
+                break;
+            case R.id.bindService:
+                bindService(intent, this, Context.BIND_AUTO_CREATE);
+                break;
+            case R.id.unBindService:
+                unbindService(this);
+                break;
+        }
+
+    }
+
+    @Override
+    public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+        System.out.println("service connected");
+    }
+
+    @Override
+    public void onServiceDisconnected(ComponentName componentName) {
 
     }
 }
--- app/src/main/java/com/example/xuepeng/learnservice/MyService.java
+++ app/src/main/java/com/example/xuepeng/learnservice/MyService.java
@@ -2,6 +2,7 @@ package com.example.xuepeng.learnservice;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
 
 public class MyService extends Service {
@@ -31,8 +32,7 @@ public class MyService extends Service {
 
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return new Binder();
 
     }
 }
